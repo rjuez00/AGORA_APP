b122e5850aa801c90a8cc2b186ff5211532e01fe
rjuez00 <rodrigo.juezh@estudiante.uam.es>
2022-09-01 20:36:10 +0200

    now installation works perfectly

--- installationScripts/exe_creation.bat
+++ installationScripts/exe_creation.bat
@@ -8,12 +8,11 @@ cd Executable
 
 
 
-pyinstaller --onedir --noconsole --add-data="../../auxFiles/*;auxFiles" --icon="../../auxFiles/appLogo.ico" --hidden-import=tqdm  --hidden-import="pkg_resources.py2_warn" --hidden-import="googleapiclient" --hidden-import="apiclient" --hidden-import=tensorflow --hidden-import=pytorch --collect-data tensorflow --collect-data torch --copy-metadata tensorflow --copy-metadata torch --copy-metadata tqdm --copy-metadata regex --copy-metadata sacremoses --copy-metadata requests --copy-metadata packaging --copy-metadata filelock --copy-metadata numpy --copy-metadata tokenizers --copy-metadata importlib_metadata --hidden-import="sklearn.utils._cython_blas" --hidden-import="sklearn.neighbors.typedefs" --hidden-import="sklearn.neighbors.quad_tree" --hidden-import="sklearn.tree._utils" --hidden-import="sklearn.utils._typedefs" --hidden-import="sklearn.neighbors._partition_nodes" ../../AGORA.py
+pyinstaller --onedir --noconsole --icon="../../auxFiles/appLogo.ico" --hidden-import=tqdm  --hidden-import="pkg_resources.py2_warn" --hidden-import="googleapiclient" --hidden-import="apiclient" --hidden-import=tensorflow --hidden-import=pytorch --collect-data tensorflow --collect-data torch --copy-metadata tensorflow --copy-metadata torch --copy-metadata tqdm --copy-metadata regex --copy-metadata sacremoses --copy-metadata requests --copy-metadata packaging --copy-metadata filelock --copy-metadata numpy --copy-metadata tokenizers --copy-metadata importlib_metadata --hidden-import="sklearn.utils._cython_blas" --hidden-import="sklearn.neighbors.typedefs" --hidden-import="sklearn.neighbors.quad_tree" --hidden-import="sklearn.tree._utils" --hidden-import="sklearn.utils._typedefs" --hidden-import="sklearn.neighbors._partition_nodes" ../../AGORA.py
 
-
-: Xcopy ..\..\auxFiles dist\auxFiles /I
+mkdir dist\AGORA\auxFiles
+Xcopy ..\..\auxFiles dist\AGORA\auxFiles /E
 
 cd ..
-
 
 iscc installator_generator.iss
